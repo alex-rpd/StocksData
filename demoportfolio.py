c6c4d14e79be595cb0a938e58a721e15ef10ffcb
Alex Rpd <rapidisalex@gmail.com>
2020-06-10 00:35:19 +0100

    made the demo portfolio separate file

---
+++ demoportfolio.py
@@ -0,0 +1,5 @@
+import datetime as dt
+
+
+demo_portfolio = {'AAPL': [{'action':'buy', 'date': dt.date(year=2010, month=1, day=1), 'amount': 2000}],
+                  'MSFT': [{'action':'buy', 'date': dt.date(year=2000, month=1, day=1), 'amount': 2000}]}
